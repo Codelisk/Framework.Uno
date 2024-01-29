245dd100e1a9a0bdb73b72f053668299eee3b89f
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-01-29 17:06:01 +0100

    Update

--- sample/Sample/Sample/Sample/Presentation/BodyView.cs
+++ sample/Sample/Sample/Sample/Presentation/BodyView.cs
@@ -11,6 +11,11 @@ public partial class BodyView : RegionBasePage
     public BodyView()
     {
         this.SetupPage<HeaderViewModel>((page, vm) => page.Content(new StackPanel().Children(
-                       new TextBlock().Text("Header"))));
+            new Grid().Children(
+                new Card().Style(StaticResource.Get<Style>("ElevatedCardStyle")).MaxWidth(200).MaxHeight(200)
+                .HeaderContent("Test")
+                .SubHeaderContent("Test 2")
+                .MediaContent("ms-appx:///Assets/salzburg.png")
+                .MediaContentTemplate(() => new Image().Width(500).HorizontalAlignment(HorizontalAlignment.Stretch).Stretch(Stretch.UniformToFill).Source(x=>x.Bind()))))));
     }
 }
