e31d000c90e0f0b0a1bc402de3d6259b105cc6d9
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-05 18:15:58 +0100

    Fix scrollview

--- src/Framework.Base/Framework.UnoNative.Shared/Shell/Shell.cs
+++ src/Framework.Base/Framework.UnoNative.Shared/Shell/Shell.cs
@@ -3,19 +3,17 @@ public sealed partial class Shell : Page
     public Shell()
     {
         this
-            .Content(new StackPanel()
-            .VerticalAlignment(VerticalAlignment.Stretch)
-            .HorizontalAlignment(HorizontalAlignment.Stretch)
-            .Children(
+            .Content(
                 new Grid().HorizontalAlignment(HorizontalAlignment.Stretch).RowDefinitions("auto,*").ColumnDefinitions("*")
                 .Children(
                     new ContentControl().VerticalAlignment(VerticalAlignment.Top).VerticalContentAlignment(VerticalAlignment.Top).HorizontalContentAlignment(HorizontalAlignment.Stretch).HorizontalAlignment(HorizontalAlignment.Stretch)
                     .Name("Header")
                     .RegionManager(regionName:"HeaderRegion"),
-                    new ContentControl().HorizontalContentAlignment(HorizontalAlignment.Stretch).HorizontalAlignment(HorizontalAlignment.Stretch)
+                    new ScrollViewer().Content(new ContentControl().HorizontalContentAlignment(HorizontalAlignment.Stretch).HorizontalAlignment(HorizontalAlignment.Stretch)
                     .Name("Body")
                     .RegionManager(regionName: "BodyRegion")
+                    )
                     .Grid(row: 1)
-            )));
+            ));
     }
 }
