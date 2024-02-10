0fda975dce44669867d8f2b40ca1096595d72f50
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-10 22:56:12 +0100

    Update

--- src/Framework.Base/Framework.UnoNative.Shared/Shell/Shell.cs
+++ src/Framework.Base/Framework.UnoNative.Shared/Shell/Shell.cs
@@ -8,7 +8,6 @@ public sealed partial class Shell : Page
                 .Children(
                     new ContentControl().VerticalAlignment(VerticalAlignment.Top).VerticalContentAlignment(VerticalAlignment.Top).HorizontalContentAlignment(HorizontalAlignment.Stretch).HorizontalAlignment(HorizontalAlignment.Stretch)
                     .Name("Header")
-                    .Margin(0,0,0,5)
                     .RegionManager(regionName:"HeaderRegion"),
                     new ScrollViewer().Content(new ContentControl().HorizontalContentAlignment(HorizontalAlignment.Stretch).HorizontalAlignment(HorizontalAlignment.Stretch)
                     .Name("Body")
