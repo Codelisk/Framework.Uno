6462a06aeb06cc17d99f379d8e593d83900bef5d
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-10-29 07:48:40 +0100

    Minor

--- src/Framework.Base/Framework.ApiClient/Models/AuthPayload.cs
+++ src/Framework.Base/Framework.ApiClient/Models/AuthPayload.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +6,5 @@ using System.Threading.Tasks;
 
 namespace Framework.ApiClient.Models
 {
-    public class AuthPayload
-    {
-        public string email { get; set; }
-        public string password { get; set; }
-    }
+    public record AuthPayload(string Email, string Password);
 }
--- src/Framework.Base/Framework.ApiClient/Models/AuthResult.cs
+++ src/Framework.Base/Framework.ApiClient/Models/AuthResult.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,10 @@ using System.Threading.Tasks;
 
 namespace Framework.ApiClient.Models
 {
-    public class AuthResult
-    {
-        public string tokenType { get; set; }
-        public string accessToken { get; set; }
-        public int expiresIn { get; set; }
-        public string refreshToken { get; set; }
-    }
+    public record AuthResult(
+        string TokenType,
+        string AccessToken,
+        int ExpiresIn,
+        string RefreshToken
+    );
 }
